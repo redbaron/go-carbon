9d407fba8b6b02e89760fb96e3f5166f3ccb068f
lomik <r.lomonosov@gmail.com>
2015-06-23 22:45:32 +0300

    Table-based cache settings tests

--- cache/cache_settings_test.go
+++ cache/cache_settings_test.go
@@ -9,88 +9,85 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type SettingsTestCase struct {
+	FieldName string
+	Valid     []interface{}
+	Invalid   []interface{}
+	Setter    func(interface{}, interface{}) // param: settings, newValue
+	Validate  func(interface{}, interface{}) // param: cache, newValue
+}
+
 func TestCacheSettings(t *testing.T) {
 	assert := assert.New(t)
 
-	testMaxSize := func(cache *Cache) {
-		table := []int{0, 1, 100, 10000000}
-
-		for _, value := range table {
-			logging.Test(func(log *bytes.Buffer) {
-				cache.EditSettings(func(settings *Settings) {
-					settings.MaxSize = value
-				})
-
-				assert.Contains(log.String(), fmt.Sprintf("new=%#v", value))
-			})
-
-			assert.Equal(value, cache.settings.MaxSize)
-		}
+	table := []SettingsTestCase{
+		SettingsTestCase{
+			"MaxSize",
+			[]interface{}{0, 1, 100, 10000000},
+			[]interface{}{},
+			func(setting interface{}, value interface{}) {
+				setting.(*Settings).MaxSize = value.(int)
+			},
+			nil,
+		},
+		SettingsTestCase{
+			"GraphPrefix",
+			[]interface{}{"carbon", "graphite"},
+			[]interface{}{},
+			func(setting interface{}, value interface{}) {
+				setting.(*Settings).GraphPrefix = value.(string)
+			},
+			nil,
+		},
+		SettingsTestCase{
+			"InputCapacity",
+			[]interface{}{0, 2, 200, 20000000},
+			[]interface{}{},
+			func(setting interface{}, value interface{}) {
+				setting.(*Settings).InputCapacity = value.(int)
+			},
+			func(cache interface{}, value interface{}) {
+				assert.Equal(value.(int), cap(cache.(*Cache).In().Chan()))
+			},
+		},
+		SettingsTestCase{
+			"OutputCapacity",
+			[]interface{}{0, 3, 300, 30000000},
+			[]interface{}{},
+			func(setting interface{}, value interface{}) {
+				setting.(*Settings).OutputCapacity = value.(int)
+			},
+			func(cache interface{}, value interface{}) {
+				assert.Equal(value.(int), cap(cache.(*Cache).Out().Chan()))
+			},
+		},
 	}
 
-	testGraphPrefix := func(cache *Cache) {
-		table := []string{"carbon", "graphite"}
-
-		for _, value := range table {
-			logging.Test(func(log *bytes.Buffer) {
-				cache.EditSettings(func(settings *Settings) {
-					settings.GraphPrefix = value
-				})
-
-				assert.Contains(log.String(), fmt.Sprintf("new=%#v", value))
-			})
-
-			assert.Equal(value, cache.settings.GraphPrefix)
+	test := func(isRunning bool) {
+		cache := New()
+		if isRunning {
+			cache.Start()
+			defer cache.Stop()
 		}
-	}
 
-	testInputCapacity := func(cache *Cache) {
-		table := []int{0, 1, 100, 10000000}
+		for _, testCase := range table {
+			for _, value := range testCase.Valid {
+				logging.Test(func(log *bytes.Buffer) {
+					cache.EditSettings(func(settings *Settings) {
+						testCase.Setter(settings, value)
+					})
 
-		for _, value := range table {
-			logging.Test(func(log *bytes.Buffer) {
-				cache.EditSettings(func(settings *Settings) {
-					settings.InputCapacity = value
+					assert.Contains(log.String(), fmt.Sprintf("new=%#v", value))
+					if testCase.Validate != nil {
+						testCase.Validate(cache, value)
+					}
 				})
-
-				assert.Contains(log.String(), fmt.Sprintf("new=%#v", value))
-			})
-
-			assert.Equal(value, cache.settings.InputCapacity)
-			assert.Equal(value, cap(cache.In().Chan()))
-		}
-	}
-
-	testOutputCapacity := func(cache *Cache) {
-		table := []int{0, 1, 100, 10000000}
-
-		for _, value := range table {
-			logging.Test(func(log *bytes.Buffer) {
-				cache.EditSettings(func(settings *Settings) {
-					settings.OutputCapacity = value
-				})
-
-				assert.Contains(log.String(), fmt.Sprintf("new=%#v", value))
-			})
-
-			assert.Equal(value, cache.settings.OutputCapacity)
-			assert.Equal(value, cap(cache.Out().Chan()))
+			}
 		}
 	}
 
 	// stopped and running
-	for _, running := range []bool{false, true} {
-		func() {
-			cache := New()
-			if running {
-				cache.Start()
-				defer cache.Stop()
-			}
-
-			testMaxSize(cache)
-			testGraphPrefix(cache)
-			testInputCapacity(cache)
-			testOutputCapacity(cache)
-		}()
+	for _, isRunning := range []bool{false, true} {
+		test(isRunning)
 	}
 }
